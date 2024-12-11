ac0856f2cf9e51a3ec0edae2bb90fe2ec71aea5f
Leandro Lemes <leandroolemes22@gmail.com>
2024-12-10 23:22:19 -0300

    remoção logo

--- src/comum/componentes/Cabecalho/Cabecalho.jsx
+++ src/comum/componentes/Cabecalho/Cabecalho.jsx
@@ -38,12 +38,57 @@
 // export default Cabecalho;
 
 
+// import { Link, useNavigate } from 'react-router-dom';
+// import Avatar from '../Avatar/Avatar';
+// import './Cabecalho.css';
+// import ServicoAutenticacao from '../../servicos/ServicoAutenticacao';
+// import { toast } from 'react-toastify';
+// import logo from "../assets/logo.svg";
+// import { useState } from 'react';
+
+// const instanciaServicoAutenticacao = new ServicoAutenticacao();
+
+// function Cabecalho() {
+//   const navigate = useNavigate();
+//   const [carregando, setCarregando] = useState(false);
+
+//   const usuarioLogado = instanciaServicoAutenticacao.buscarUsuarioLogado();
+
+//   const sair = async () => {
+//     setCarregando(true);
+//     await instanciaServicoAutenticacao.sair();
+//     toast.success('Você saiu com sucesso!');
+//     setCarregando(false);
+//     navigate('/login');
+//   };
+
+//   return (
+//     <header className="cabecalho_root">
+//       <Link to="/">
+//         <img src={logo} alt="Logo" height={20} />
+//       </Link>
+
+//       {usuarioLogado && (
+//         <div className="cabecalho_usuario">
+//           <Link to="/meu-perfil">
+//             <Avatar nome={usuarioLogado.nome} />
+//           </Link>
+//           <button className="cabecalho_sair" onClick={sair} disabled={carregando}>
+//             {carregando ? 'Saindo...' : 'Sair'}
+//           </button>
+//         </div>
+//       )}
+//     </header>
+//   );
+// }
+
+// export default Cabecalho;
+
 import { Link, useNavigate } from 'react-router-dom';
 import Avatar from '../Avatar/Avatar';
 import './Cabecalho.css';
 import ServicoAutenticacao from '../../servicos/ServicoAutenticacao';
 import { toast } from 'react-toastify';
-import logo from '../../assets/logo.svg';
 import { useState } from 'react';
 
 const instanciaServicoAutenticacao = new ServicoAutenticacao();
@@ -64,9 +109,7 @@ function Cabecalho() {
 
   return (
     <header className="cabecalho_root">
-      <Link to="/">
-        <img src={logo} alt="Logo" height={20} />
-      </Link>
+      <Link to="/">Início</Link>
 
       {usuarioLogado && (
         <div className="cabecalho_usuario">
