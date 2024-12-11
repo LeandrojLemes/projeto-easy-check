602f7644bd705db1de76894ba59bbad49afdfb7f
Leandro Lemes <leandroolemes22@gmail.com>
2024-12-11 18:22:43 -0300

    logo atualizada novamente

--- vite.config.js
+++ vite.config.js
@@ -23,13 +23,13 @@ export default defineConfig({
         icons: [
           {
             // src: 'icons/icon-192x192.png',
-            src: 'icons/logoEasyCheck.jpeg',
+            src: 'icons/logoEasyCheck.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
             // src: 'icons/icon-512x512.png',
-            src: 'icons/logoEasyCheck.jpeg',
+            src: 'icons/logoEasyCheck.png',
             sizes: '512x512',
             type: 'image/png',
           },
