8366cca986fac0002f9a5c87acada23b0066ac8b
Leandro Lemes <leandroolemes22@gmail.com>
2025-06-16 20:23:03 -0300

    Alterada logo

--- vite.config.js
+++ vite.config.js
@@ -16,19 +16,19 @@ export default defineConfig({
       manifest: {
         name: 'Easy Check',
         short_name: 'Easy Check',
-        description: 'É um projeto para o cadastro e a verificação ágil de documentos de colaboradores de uma certa empresa!',
+        description: 'É um projeto para o cadastro e gerenciamento de colaboradores e seus documentos',
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
         icons: [
           {
-            // src: 'icons/icon-192x192.png',
+            
             src: 'icons/logoEasyCheck192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            // src: 'icons/icon-512x512.png',
+            
             src: 'icons/logoEasyCheck512.png',
             sizes: '512x512',
             type: 'image/png',
